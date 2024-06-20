ec78f809e7cf1bf8fd4a076bd7820d6ccbdef464
Adindan1 <mavenanchavenuseris@gmail.com>
2024-06-19 20:33:29 -0400

    Added updates to navbar and images on the webpage

--- React-Test/react-test/src/containers/header/Header.jsx
+++ React-Test/react-test/src/containers/header/Header.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import './header.css';
+import people from '../../assets/people.png'
+import ai from '../../assets/ai.png'
+import './header.css';
 
-const Header = () => {
-    return (
-        <div>
-            Header
+const Header = () => (    
+    <div className='gpt3__header section__padding' id='home'>
+    <div className='gpt3__header-content'>
+        <h1 className='gradient__text'>Let's build something amazing with GPT-3 OpenAI</h1>
+        <p>GPT-3 is an amazing resource to getting started familiarizing yourself with AI technology's capabilities.</p>
+
+        <div className='gpt3__header-content__input'>
+            <input type='email' placeholder='Your Email Address'></input>
+            <button type='button'>Get Started</button>
         </div>
-    )
-}
 
-export default Header
+        <div className='gpt3__header-content__people'>
+            <img src={people} alt='people'/>
+            <p>1,743 people have visited this site in the past 24 hours</p>
+        </div>
+    </div>
+    <div className='gpt3__header-image'>
+        <img src={ai} />
+    </div>
+    </div>
+);
+
+export default Header;
